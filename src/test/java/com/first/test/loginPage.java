f190c8148dd316e7cb707269ea84732912d2c467
Bendakayala Jamal Basha <bjamalbasha786@gmail.com>
2018-12-23 01:50:37 +0530

    first commit

---
+++ src/test/java/com/first/test/loginPage.java
@@ -0,0 +1,14 @@
+package com.first.test;
+
+
+import org.testng.annotations.Test;
+
+public class loginPage {
+	
+	@Test
+	public void loginPage()
+	{
+		System.out.println("jamal basha");
+		
+	}
+	}
